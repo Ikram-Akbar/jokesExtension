5ba5d01946996c0b04b0ee11cf15ef258754ae1a
Ikram-Git-Coder <ikramakbar71@gmail.com>
2022-05-25 10:29:11 +0600

    update the file

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-fetch('https://icanhazdadjoke.com/slack')
-    .then(data => data.json())
-    .then(jokeData => {
-        const jokeText = jokeData.attachments[0].text;
-        const jokeElement = document.getElementById('jokeElement');
+document.getElementById("btn_1").addEventListener("click", clickCounter);
+document.getElementById("btn_2").addEventListener("click", clickCounter2);
 
-        jokeElement.innerHTML = jokeText;
-    })
+function clickCounter() {
+  if (typeof Storage !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount) + 1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("result").innerHTML =
+      "Result : " + localStorage.clickcount;
+  }
+}
+
+function clickCounter2() {
+  if (typeof Storage !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount) - 1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("result").innerHTML =
+      "Result :" + localStorage.clickcount;
+  }
+}
